30e9c9eb49ee5b261a1200598c1901a3239db9a1
Nils Homer <nilshomer@gmail.com>
2018-08-14 14:38:53 -0700

    Adding a StringUtil.split method for faster String splitting

--- src/main/scala/com/fulcrumgenomics/commons/util/StringUtil.scala
+++ src/main/scala/com/fulcrumgenomics/commons/util/StringUtil.scala
@@ -150,8 +150,37 @@ object StringUtil {
       }.mkString("\n")
     }
     catch {
-      case ex: java.lang.IndexOutOfBoundsException =>
+      case _: java.lang.IndexOutOfBoundsException =>
         throw new IllegalArgumentException("columnIt failed.  Did you forget to input an NxM table?")
     }
+  }
+
+  /** Splits line on delimiter, placing up to `arr.length` values into `arr` and returning the number of values.
+    *
+    * Multiple consecutive delimiters are treated as individual delimiters, delimiting empty fields. E.g. with
+    * `delimiter=','` the string `a,b,,,c` will return `["a", "b", "", "", "c"]`.
+    * */
+  def split(line: String, delimiter: Char = '\t', arr: Array[String]): Int = {
+    val cs    = line.toCharArray
+    val len   = cs.length
+    var i     = 0
+    var count = 0
+    var start = 0
+    var end   = 0
+
+    while (i <= len && count < arr.length) {
+      if (i == len || cs(i) == delimiter) {
+        arr(count) = new String(cs, start, end-start)
+        count += 1
+        start = i + 1
+        end   = i + 1
+      }
+      else {
+        end += 1
+      }
+
+      i += 1
+    }
+    count
   }
 }
