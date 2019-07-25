a2001339da07aa0696655978003969ef5d6b248c
tfenne <tfenne@tfenne.com>
2019-07-25 08:36:06 -0600

    Fix some scaladocs.

--- src/main/scala-2.12-/com/fulcrumgenomics/commons/Compat.scala
+++ src/main/scala-2.12-/com/fulcrumgenomics/commons/Compat.scala
@@ -2,11 +2,13 @@ package com.fulcrumgenomics.commons
 
 import scala.annotation.ClassfileAnnotation
 
-/** Trait that includes types used for cross-building compatibility; scala 2.13 version. */
+/** Trait that includes types used for cross-building compatibility; scala 2.12 version. */
 private[commons] trait Compat {
-  /** For annotations available at runtime in 2.13 we use ConstantAnnotation. */
+  /** For annotations available at runtime in 2.12 we use ClassfileAnnotation which has been deprecated in
+    * 2.13 and replaced with the newly defined ConstantAnnotation.. */
   type ConstantAnnotation = scala.annotation.ClassfileAnnotation
 
-  /** For basic views we use the new scala.collection.View. */
+  /** For basic views in 2.12 we use the old scala.collection.IterableView which has disappeared in 2.13 to
+    * be replaced by View. */
   type View[+A] = scala.collection.IterableView[A, Iterable[A]]
 }
