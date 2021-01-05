cd3be9ab06249a0b0d57af69f06c897cb8fcb36a
Jay Carey <jay@fulcrumgenomics.com>
2021-01-05 13:22:20 -0500

    Review changes.

--- src/main/scala/com/fulcrumgenomics/commons/io/PathUtil.scala
+++ src/main/scala/com/fulcrumgenomics/commons/io/PathUtil.scala
@@ -28,8 +28,8 @@ import java.nio.file.{Path, Paths}
 
 /** Provides utility methods for creating and manipulating Path objects and path-like Strings. */
 object PathUtil {
-  val maxFileNameSize: Int = 254
-  val illegalCharacters: String = "[!\"#$%&'()*/:;<=>?@\\^`{|}~] "
+  val MaxFileNameSize: Int = 254
+  val IllegalCharacters: String = "[!\"#$%&'()*/:;<=>?@\\^`{|}~] "
 
   /** Resolves a path from a String, and then makes the path absolute. Prefer this to PathUtil.pathTo elsewhere. */
   def pathTo(first: String, more: String*): Path = Paths.get(first, more:_*).toAbsolutePath.normalize
@@ -38,19 +38,19 @@ object PathUtil {
     * truncate the file name to be, at maximum, 255 characters.
     *
     * @param fileName the string that is to be used as a filename
-    * @param illegalCharacters the set of characters to be replaced if found, defaults to [[illegalCharacters]]
+    * @param illegalCharacters the set of characters to be replaced if found, defaults to [[IllegalCharacters]]
     * @param replacement an optional replacement character, defaulting to '_'; if None characters are just removed
     * @return the filename without illegal characters
     */
   def sanitizeFileName(fileName: String,
-                       illegalCharacters: String = PathUtil.illegalCharacters,
+                       illegalCharacters: String = PathUtil.IllegalCharacters,
                        replacement: Option[Char] = Some('_'),
                        maxFileNameSize: Option[Int] = Some(MaxFileNameSize)): String = {
     val sanitizedFileName = replacement match {
       case None    => fileName.filter(c => !illegalCharacters.contains(c))
       case Some(r) => fileName.map(c => if (illegalCharacters.contains(c)) r else c)
     }
-    sanitizedFileName.substring(0, Math.min(sanitizedFileName.length, maxFileNameSize))
+    sanitizedFileName.substring(0, Math.min(sanitizedFileName.length, MaxFileNameSize))
   }
 
   /** Replaces the extension on an existing path. */
